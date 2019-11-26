fb4876928bbc6d01bf04a61782000008b054a384
github username <github email address>
2019-11-26 16:11:03 -0500

    Added settings features

--- components/Parameters.js
+++ components/Parameters.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { ScrollView, Text, View, StyleSheet } from "react-native";
+import ShowSettings from "./ShowSettings";
 
 const Parameters = ({ settings }) => {
   return (
-    <ScrollView>
-      <View style={styles.outerView}>
-        {settings.sixteenParams.map(param => {
-          return (
-            <View style={styles.paramsView} key={param}>
-              <Text style={styles.paramsText}>{param}</Text>
-            </View>
-          );
-        })}
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.viewContainer}>
+        <View style={styles.paramsView}>
+          <Text>CCU</Text>
+          {settings.sixteenParams.map(param => {
+            return (
+              <View style={styles.textView} key={param}>
+                <Text style={styles.paramsText}>{param}</Text>
+              </View>
+            );
+          })}
+        </View>
+        <ShowSettings practiceProps={settings} />
       </View>
     </ScrollView>
   );
@@ -19,14 +24,17 @@ const Parameters = ({ settings }) => {
 export default Parameters;
 
 const styles = StyleSheet.create({
-  outerView: {
+  viewContainer: {
     flex: 1,
-    flexDirection: "column"
+    flexDirection: "row"
   },
   paramsView: {
     backgroundColor: "#F2D26B",
-    height: "auto",
-    width: "100%"
+    height: "auto"
+  },
+  textView: {
+    borderBottomColor: "#000",
+    borderBottomWidth: 1
   },
   paramsText: {
     padding: 10
--- components/ShowSettings.js
+++ components/ShowSettings.js
@@ -2,11 +2,17 @@ import React from "react";
 import { StyleSheet, TouchableOpacity, Text, View } from "react-native";
 
 const ShowSettings = props => {
+  console.log("this is props:", props.practiceProps);
   return (
-    <View style={styles.settingsOuterView}>
-      <View style={styles.settingsInnerView}>
-        <Text>ShowSettings</Text>
-      </View>
+    <View style={styles.showView}>
+      <Text>CCU</Text>
+      {props.practiceProps.sixteenParams.map(setting => {
+        return (
+          <View key={setting} style={styles.textView}>
+            <Text style={styles.showText}>{setting}</Text>
+          </View>
+        );
+      })}
     </View>
   );
 };
@@ -14,28 +20,15 @@ const ShowSettings = props => {
 export default ShowSettings;
 
 const styles = StyleSheet.create({
-  settingsOuterView: {
-    width: "60%"
-  },
-  settingsInnerView: {
-    height: 200,
+  showView: {
     backgroundColor: "#F2D26B",
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 20
+    height: "auto"
   },
-  resetButton: {
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 20,
-    paddingTop: 20,
-    paddingBottom: 20,
-    height: "auto",
-    width: "100%",
-    backgroundColor: "#000",
-    opacity: 0.8
+  textView: {
+    borderBottomColor: "#000",
+    borderBottomWidth: 1
   },
-  resetText: {
-    color: "#fff"
+  showText: {
+    padding: 10
   }
 });
